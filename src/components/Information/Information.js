361b33c239373450cf3739edb18e611b6980d0fb
Easir Arafat <riyadhbd2@Easirs-MacBook-Pro.local>
2023-12-30 22:30:07 +0100

    break time added

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Information.css'
 
+
+
 const Information = ({totalTime}) => {
+    const[breakTime, setBreakTime] = useState(0);
     let total = 0;
     for (const time of totalTime) {
         total = total + time;
     }
+
+
+
+const addBreak =(e)=>{
+    setBreakTime(e.target.value);
+}
 
     return (
         <div className='all-information'>
@@ -13,16 +22,16 @@ const Information = ({totalTime}) => {
             <div>
                 <h4>Break Time in Hours</h4>
                 <div className='break-time'>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
+                    <button onClick={addBreak} value = "1">1</button>
+                    <button onClick={addBreak} value = "2">2</button>
+                    <button onClick={addBreak} value = "3">3</button>
+                    <button onClick={addBreak} value = "4">4</button>
+                    <button onClick={addBreak} value = "5">5</button>
                 </div>
                 <div>
                     <h4>Total Spending Time in Hours</h4>
                     <p>Total Time: {total}</p>
-                    <p>Break Time:</p>
+                    <p>Break Time: {breakTime}</p>
                 </div>
                 <div className='btn-done'>
                     <button>Daily Activity Done</button>
